956c2744e14b9d7cbeb31fba9ccc0abfff2d85f2
vaibhavsingh1believin-jpg <vaibhav.singh1believin@gmail.com>
2025-10-03 19:28:34 +0530

    spacing-fix

--- wp-content/themes/storefront-child/pages/template-blog.php
+++ wp-content/themes/storefront-child/pages/template-blog.php
@@ -18,7 +18,7 @@ get_header();
         <input
           type="text"
           placeholder="Search Mod Labs Blog"
-          class="w-full rounded-[10px] text-[13px] sm:text-[16px] placeholder:text-black bg-gray-2 border-black py-[4.75px] sm:py-[11px] px-4 pr-10 focus:outline-none focus:ring-1 focus:ring-black"
+          class="w-full rounded-[10px] text-[13px] sm:text-[16px] placeholder:text-black bg-gray-2 border-black py-[4.75px] sm:py-[11px] px-4 pr-10 focus:outline-none focus:ring-1 focus:ring-black tracking-[-1.08px]"
         />
         <svg class="absolute right-3 top-[28%] sm:top-[30%] w-[14px] h-[14px] sm:w-5 sm:h-5 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -35,8 +35,8 @@ get_header();
              alt="THC Edibles" 
              class="w-full h-auto object-cover" />
         <div class="pt-[17px] sm:pt-4 pb-[14px] sm:pb-6 px-8">
-          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans">6 Best THC Edibles Reviewed for 2025</h2>
-          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none">
+          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans tracking-[-1.08px]">6 Best THC Edibles Reviewed for 2025</h2>
+          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none tracking-[-1.08px]">
             The hemp edible market has evolved dramatically over the past few years, and as we approach 2025, hemp gummies are at the forefront of cannabis innovation...
           </p>
           <div class="flex justify-between items-center mt-3 sm:mt-4 text-xs sm:text-sm ">
@@ -44,8 +44,8 @@ get_header();
               <svg class="h-3 sm:w-4 sm:h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span class="text-[9px] sm:text-[12px]">Jan 2025</span>
+              </svg> 
+              <span class="text-[9px] sm:text-[12px] tracking-[-1.08px]">Jan 2025</span>
             </div>
             <a href="#" class="text-black font-medium text-[11px] sm:text-[15px] hover:underline">Read more</a>
           </div>
@@ -58,8 +58,8 @@ get_header();
              alt="THC Edibles" 
              class="w-full h-auto object-cover" />
         <div class="pt-[17px] sm:pt-4 pb-[14px] sm:pb-6 px-8">
-          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans">10 Best Cannabis-Infused Drinks for Dry</h2>
-          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none">
+          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans tracking-[-1.08px]">10 Best Cannabis-Infused Drinks for Dry</h2>
+          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none tracking-[-1.08px] ">
            The hemp edible market has evolved dramatically over the past few years, and as we approach 2025, hemp gummies are at the forefront of cannabis innovation...
           </p>
           <div class="flex justify-between items-center mt-3 sm:mt-4 text-xs sm:text-sm ">
@@ -68,7 +68,7 @@ get_header();
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span class="text-[9px] sm:text-[12px]">Jan 2025</span>
+              <span class="text-[9px] sm:text-[12px] tracking-[-1.08px]">Jan 2025</span>
             </div>
             <a href="#" class="text-black font-medium text-[11px] sm:text-[15px] hover:underline">Read more</a>
           </div>
@@ -81,8 +81,8 @@ get_header();
              alt="THC Edibles" 
              class="w-full h-auto object-cover" />
         <div class="pt-[17px] sm:pt-4 pb-[14px] sm:pb-6 px-8">
-          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans">Hemp Gummies for Self-Care This Valentine’s Day</h2>
-          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none">
+          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans tracking-[-1.08px]">Hemp Gummies for Self-Care This Valentine’s Day</h2>
+          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none tracking-[-1.08px] ">
             Valentine’s Day often comes with pressure to celebrate grand romantic gestures. But this year, why not shift the focus inward? Self-care is one of the purest forms of love...
           </p>
           <div class="flex justify-between items-center mt-3 sm:mt-4 text-xs sm:text-sm ">
@@ -91,7 +91,7 @@ get_header();
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span class="text-[9px] sm:text-[12px]">Jan 2025</span>
+              <span class="text-[9px] sm:text-[12px] tracking-[-1.08px]">Jan 2025</span>
             </div>
             <a href="#" class="text-black font-medium text-[11px] sm:text-[15px] hover:underline">Read more</a>
           </div>
@@ -104,8 +104,8 @@ get_header();
              alt="THC Edibles" 
              class="w-full h-auto object-cover" />
         <div class="pt-[17px] sm:pt-4 pb-[14px] sm:pb-6 px-8">
-          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans">6 Best THC Edibles Reviewed for 2025</h2>
-          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none">
+          <h2 class="text-base sm:text-lg text-[15px] sm:text-[20px] font-semibold font-sans tracking-[-1.08px]">6 Best THC Edibles Reviewed for 2025</h2>
+          <p class="text-xs sm:text-[15px] mt-3 line-clamp-2 sm:line-clamp-none tracking-[-1.08px] ">
             The hemp edible market has evolved dramatically over the past few years, and as we approach 2025, hemp gummies are at the forefront of cannabis innovation...
           </p>
           <div class="flex justify-between items-center mt-3 sm:mt-4 text-xs sm:text-sm ">
@@ -114,7 +114,7 @@ get_header();
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span class="text-[9px] sm:text-[12px]">Jan 2025</span>
+              <span class="text-[9px] sm:text-[12px] tracking-[-1.08px]">Jan 2025</span>
             </div>
             <a href="#" class="text-black font-medium text-[11px] sm:text-[15px] hover:underline">Read more</a>
           </div>
